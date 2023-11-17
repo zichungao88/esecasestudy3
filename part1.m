29388d9487ae3b05abdfc6cef7f490f80a9b4ed5
Zichun (Jerry) Gao <zichungao88@gmail.com>
2023-11-17 16:44:34 -0600

    Finish Part 1 Lab Exercise

--- part1.m
+++ part1.m
@@ -3,27 +3,40 @@
 
 % Ray Propagation in Free Space
 
+n = 20; % # of rays
 d = 0.2; % 0.2 m or 200 mm
 s = -pi / 20;
 a = (pi / 20 - s) / 9;
+angles = linspace(-pi / 20, pi / 20, n);
 
-rays_in1 = [0,      0,      0,      0,      0,      0,      0,      0;
-            s+a,    s+2*a,  s+3*a,  s+4*a,  s+5*a,  s+6*a,  s+7*a,  s+8*a;
-            0,      0,      0,      0,      0,      0,      0,      0;
-            0,      0,      0,      0,      0,      0,      0,      0];
+rays_in1 = nan(4, n); % starting point: (0, 0, 0)
+for i = 1:4
+    if i == 2
+        rays_in1(i, :) = angles;
+    else
+        rays_in1(i, :) = zeros;
+    end
+end
 
-rays_in2 = [0.01,   0.01,   0.01,   0.01,   0.01,   0.01,   0.01,   0.01;
-            s+a,    s+2*a,  s+3*a,  s+4*a,  s+5*a,  s+6*a,  s+7*a,  s+8*a;
-            0,      0,      0,      0,      0,      0,      0,      0;
-            0,      0,      0,      0,      0,      0,      0,      0];
+rays_in2 = nan(4, n); % starting point: (0.01, 0, 0)
+for i = 1:4
+    if i == 1
+        rays_in2(i, :) = zeros + 0.01;
+    elseif i == 2
+        rays_in2(i, :) = angles;
+    else
+        rays_in2(i, :) = zeros;
+    end
+end
 
 Md = [1,     d,      0,      0;
-     0,     1,      0,      0;
-     0,     0,      1,      d;
-     0,     0,      0,      1];
+      0,     1,      0,      0;
+      0,     0,      1,      d;
+      0,     0,      0,      1];
 
 rays_out1 = Md * rays_in1;
 ray_z1 = [zeros(1, size(rays_in1, 2)); d * ones(1, size(rays_in1, 2))];
+
 rays_out2 = Md * rays_in2;
 ray_z2 = [zeros(1, size(rays_in2, 2)); d * ones(1, size(rays_in2, 2))];
 
@@ -61,21 +74,32 @@ Mf = [1,    0,      0,      0;
       0,    0,      -1/f,   0];
 
 % Md2 * (Mf * (Md1 * rays_in))
-rays_in1a = rays_in1;
-rays_out1a = Md1 * rays_in1a;
+rays_in1a = rays_in1; % (0, 0, 0)
+rays_out1a = Md1 * rays_in1a; % pre-lense phase
 rays_z1a = [zeros(1, size(rays_in1a, 2)); d1 * ones(1, ...
     size(rays_in1a, 2))];
-rays_in1b = rays_out1a;
-rays_out1b = Mf * rays_in1b;
+rays_in1b = [];
+for i = 1:n % only keep rays that hit the lense
+    if abs(rays_out1a(1, i)) <= r
+        rays_in1b = [rays_in1b, rays_out1a(:, i)];
+    end
+end
+rays_out1b = Mf * rays_in1b; % passing through the lense
 rays_in1c = rays_out1b;
-rays_out1c = Md2 * rays_in1c;
+rays_out1c = Md2 * rays_in1c; % post-lense phase
 rays_z1c = [zeros(1, size(rays_in1c, 2)); d2 * ones(1, ...
     size(rays_in1c, 2))];
-rays_in2a = rays_in2;
+
+rays_in2a = rays_in2; % (0.01, 0, 0)
 rays_out2a = Md1 * rays_in2a;
 rays_z2a = [zeros(1, size(rays_in2a, 2)); d1 * ones(1, ...
     size(rays_in2a, 2))];
-rays_in2b = rays_out2a;
+rays_in2b = [];
+for i = 1:n
+    if abs(rays_out2a(1, i)) <= r
+        rays_in2b = [rays_in2b, rays_out2a(:, i)];
+    end
+end
 rays_out2b = Mf * rays_in2b;
 rays_in2c = rays_out2b;
 rays_out2c = Md2 * rays_in2c;
@@ -96,5 +120,3 @@ hold off;
 title('Ray Propagation through Lenses');
 xlabel('z (m)');
 ylabel('x (m)');
-
-% TODO: Add "lost" rays past the lense
